5ff20c6f176e4d53ce681c38488e66de51fb2da0
Ronnie <ronnie94official@gmail.com>
2024-04-30 17:25:54 +0100

    Update MarkProductAsUnsaleableCommand.java

--- command-side/src/main/java/com/soagrowers/productcommand/commands/MarkProductAsUnsaleableCommand.java
+++ command-side/src/main/java/com/soagrowers/productcommand/commands/MarkProductAsUnsaleableCommand.java
@@ -12,12 +12,6 @@ public class MarkProductAsUnsaleableCommand {
     @TargetAggregateIdentifier
     private final String id;
 
-    /**
-     * This constructor must set the Id field, otherwise it's unclear
-     * to Axon which aggregate this command is intended for.
-     *
-     * @param id
-     */
     public MarkProductAsUnsaleableCommand(String id) {
         this.id = id;
     }
